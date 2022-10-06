04dd8f0209d436ff4b9cb286f4d5a2ff7c5b872c
anusha0004 <115171603+anusha0004@users.noreply.github.com>
2022-10-06 08:25:12 -0700

    Add files via upload

---
+++ managedDiskMigration.ps1
@@ -0,0 +1,22 @@
+﻿#Source storage account
+$rgName ="RG_NAME"
+$location ="RG_LOCATION"
+$storageAccountName ="STORAGE_ACC_NAME"
+$diskName ="DISK_NAME"
+
+#Target storage account
+$destrgName ="DESTINATION RG NAME"
+$destlocation ="RG LOCATION"
+$deststorageAccountName ="STORAGE ACC NAME"
+$destdiskName ="SELECT DEST DISK NAME YOU WANT TO SAVE DISK AS"
+ 
+#Assign access to the source disk
+$sas =Grant-AzDiskAccess -ResourceGroupName $rgName -DiskName $diskName -DurationInSecond 3600 -Access Read
+
+$saKey =Get-AzStorageAccountKey -ResourceGroupName $destrgName -Name $deststorageAccountName
+$storageContext =New-AzStorageContext –StorageAccountName $deststorageAccountName -StorageAccountKey $saKey[0].Value
+New-AzStorageContainer -Context $storageContext -Name "CONTAINER NAME"
+
+Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer "CONTAINER NAME" -DestContext $storageContext -DestBlob $destdiskName
+
+Get-AzStorageBlobCopyState -Context $storageContext -Blob $destdiskName -Container "CONTAINER NAME"
